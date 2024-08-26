00af0cbb84eec6d3f341a2dec28c9e2809ba7642
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-08-26 16:09:29 +0100

    .

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,25 @@ import (
 	"log"
 	"net/http"
 
+	"backend/config"
 	"backend/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+
+	err := config.InitDB()
+	if err != nil {
+		log.Fatalf("error connecting to database : %v", err)
+
+	}
+
 	r := mux.NewRouter()
 	routes.RegisterRoutes()
 
 	fmt.Println("server is starting on port 8080")
 
-	err := http.ListenAndServe(":8080", r)
+	err = http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatal(err)
 	}
