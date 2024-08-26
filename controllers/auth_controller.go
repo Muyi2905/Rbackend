350ed8326d407ee8623a1a861f3949b037fb2824
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-08-26 14:49:32 +0100

    update

---
+++ controllers/auth_controller.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"backend/models"
+	"encoding/json"
+	
+	"net/http"
+)
+
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-type", "Application/json")
+	var registerData models.Register
+	err := json.NewDecoder(r.Body).Decode(&registerData)
+	if err != nil {
+		http.Error(w, "Invalid Input", http.StatusBadRequest)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("user logged in successfully"))
+}
--- models/models.go
+++ models/models.go
@@ -1,13 +1,15 @@
 package models
 
 type Register struct {
+	ID        string `json:"id"`
 	FirstName string `json:"firstname"`
 	LastName  string `json:"lastname"`
 	Email     string `json:"email"`
 	Password  string `json"password "`
-	UserName string `json:"username"`
+	UserName  string `json:"username"`
 }
 type Login struct {
+	Id        string `json:"id"`
 	FirstName string `json:"firstname"`
 	LastName  string `json:"lastname"`
 	Password  string `json"password "`
