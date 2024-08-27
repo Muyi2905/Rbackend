df291ddfd0a94c81d5e90089bb74a44fccc6a900
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-08-27 12:36:46 +0100

    updated route.go

--- controllers/users_controller.go
+++ controllers/users_controller.go
@@ -1,1 +1,45 @@
-p
+package controllers
+
+import (
+	"backend/models"
+	"encoding/json"
+	"net/http"
+
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	users, err := models.FetchAllUsers()
+	if err != nil {
+		http.Error(w, "failed to get users", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+func GetUserById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "inavild Id", http.StatusBadRequest)
+		return
+	}
+
+	user, err := models.FetchUserByID(id)
+	if err != nil {
+		if err.Error() == "user not found" {
+			http.Error(w, "user not found", http.StatusNotFound)
+		} else {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
--- routes/routes.go
+++ routes/routes.go
@@ -1,15 +1,13 @@
 package routes
 
 import (
-	"backend/routes"
-
 	"github.com/gorilla/mux"
 )
 
 func RegisterRoutes() *mux.Router {
 	r := mux.NewRouter()
 
-	routes.RegisterAuthRoutes(r)
-	routes.UserRoutes()
+	RegisterAuthRoutes(r)
+	UserRoutes()
 	return r
 }
