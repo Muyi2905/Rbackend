aab6b8abf9d95e2b9fcb4be8981bfd81de8fe14a
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-09-02 10:42:14 +0100

    jwt

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,9 +1,19 @@
 package utils
 
 import (
-	""
+	"go/token"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
-func ()  {
-	
+func CreateToken(UserId string) (string, error) {
+	claims:= jwt.Claims{
+		"userId" : UserId,
+		"exp" :    time.Now().Add(time.Hour*24).Unix()
+	}
+
+	token:= jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET_KEY")))
 }
